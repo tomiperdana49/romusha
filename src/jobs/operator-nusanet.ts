94227d697fee6c5758f1011f4b5d4226f462bb40
Tomi <tomi@nusa.net.id>
2025-10-07 13:42:43 +0700

    feat(mysql): add DBAIS5 MySQL connection and syncDataCgsToDba job handler
    - Added new DBAIS5 MySQL config variables to config.ts
    - Created dbaIs5 connection pool in nis.mysql.ts
    - Integrated syncDataCgsToDba job handler in processor.ts

---
+++ src/jobs/operator-nusanet.ts
@@ -0,0 +1,36 @@
+import { pool as mysqlDb, dbaIs5 as mysqlDbaIs5 } from '../nis.mysql'
+
+export async function syncDataCgsToDba() {
+  const [dataOperatorCgs] = await mysqlDb.query(
+    `
+            SELECT
+                cs.custServId,
+                MAX(CASE WHEN cstc.attribute = 'Vendor CID' THEN cstc.value END) AS 'vendorCid',
+                MAX(CASE WHEN cstc.attribute = 'ONU IP'     THEN cstc.value END) AS 'onuIp'
+            FROM Customer c
+            LEFT JOIN CustomerServices cs ON c.CustId = cs.CustId
+            LEFT JOIN CustomerServiceTechnicalLink cstl ON cs.custServId = cstl.custServId
+            LEFT JOIN CustomerServiceTechnicalCustom cstc ON cstc.technicalTypeId = cstl.id
+                AND cstc.attribute REGEXP 'Vendor CID|ONU IP'
+            WHERE c.BranchId = '020'
+                AND cs.CustStatus <> 'NA'
+                AND cstl.foVendorId = 2
+            GROUP BY
+                cs.custServId, cs.CustStatus, cstl.foVendorId, cstl.id
+            ORDER BY cs.custServId limit 1
+        `,
+  )
+  await mysqlDbaIs5.query('DELETE FROM cgs')
+  for (const cgs of dataOperatorCgs as any[]) {
+    const { custServId, vendorCid, onuIp } = cgs
+
+    try {
+      const [insRes] = await mysqlDbaIs5.query(
+        `INSERT INTO cgs (service_id, onu_ip, operator_cid) VALUES (?, ?, ?)`,
+        [custServId, onuIp, vendorCid],
+      )
+    } catch (err) {
+      console.error('insert failed:', err)
+    }
+  }
+}
